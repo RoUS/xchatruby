1e6d73b013c38da751b560b5447cd79681826686
gwon <>
2007-04-26 11:04:05 +0000

    Added MacOSX 10.4 universal makefile

--- src/xchat-ruby-plugin.rb
+++ src/xchat-ruby-plugin.rb
@@ -704,6 +704,7 @@ module XChatRuby
     end
 
     def rb_command_hook( words, words_eol, data )
+      words = "" if words == nil
       case words[1].downcase
         when "" then
           puts "You must specify the RB command to invoke"
